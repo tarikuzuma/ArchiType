a66d005b78cea333f89631c73c3eb76ac994b33c
Tarikuzuma <edwinjrgumba@gmail.com>
2023-03-13 14:43:36 +0800

    Concept update for main menu

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,15 @@
 import tkinter as gui
 from calculator import *
 
+def say_hello():
+    print("Hello!")
+
+def say_goodbye():
+    print("Goodbye!")
+
+def display_dropdown(event):
+    dropdown_menu.post(event.x_root, event.y_root)
+
 root = gui.Tk() #returns title of the screen #Parent
 root.title("Architype") #sets title of the screen
 
@@ -14,6 +23,11 @@ button1.pack(padx=20, pady= 7)
 
 button2 = gui.Button(root, text="Converter", font = ('Helvetica', 13), height= 2, width=50, fg='white', bg='gray')
 button2.pack(padx=20, pady= 7)
+
+dropdown_menu = gui.Menu(button2, tearoff=False)
+dropdown_menu.add_command(label="Say Hello", command=say_hello)
+dropdown_menu.add_command(label="Say Goodbye", command=say_goodbye)
+button2.bind("<Button-1>", display_dropdown)
 
 button3 = gui.Button(root, text="Loram Ipsum", font = ('Helvetica', 13), height= 2, width=50, fg='white', bg='gray')
 button3.pack(padx=20, pady= 7)
