d9fee4b3cad4a2510edd792e7c0e38e8cc11766d
Tarikuzuma <edwinjrgumba@gmail.com>
2023-04-04 00:39:06 +0800

    Updated CLI

--- ArchiType.py
+++ ArchiType.py
@@ -4,6 +4,53 @@
 #Special thanks to Tharchen Ozzy for the Pythagorean Theorem idea! https://www.codegrepper.com/profile/tharchen-ozzy
 import math
 
+def trianglecheck():
+    def calculate_triangle():
+        try:
+            print ("")
+            a = float(input("Input side A: "))
+            b = float(input("Input side B: "))
+            c = float(input("Input side C: "))
+
+            if a <= 0 or b <= 0 or c <= 0:
+                print("\n- Invalid input: Negative or Input is Equal to 0 -\n")
+                return
+            
+            if (a+b<=c) or (b+c<=a) or (c+a<=b):
+                print("\n- Invalid input: Triangle Inequality Theorem not Observed-\n")
+                return
+            
+            if a == b == c:
+                triangle_type = "Equilateral Triangle"
+
+            elif a==b or b==c or a==c:
+                triangle_type = "Isosceles Triangle"
+
+            else:
+                triangle_type = "Scalene Triangle"
+            
+            s = (a + b + c) / 2
+            area = math.sqrt(s*((s-a)*(s-b)*(s-c)))
+            print(f'\nIdentify the triangle_type: {area}')
+            print(f'The triangle_type of triangle is: {triangle_type}\n')
+
+        except ValueError:
+            print("- Error: Invalid Input -\n")
+
+    while True:
+        calculate_triangle()
+        while True:
+            choice = input("Do you wish to Continue? [Y/N]: ").lower()
+            if choice == 'y':
+                print("\n - Continue - \n")
+                break
+            elif choice == 'n':
+                print("\n - Good Bye - \n")
+                return
+            else:
+                print("\n - Invalid Input - \n")
+                continue
+
 def unitcon():
     print("Enter the distance to convert:")
     distance = float(input())
@@ -32,7 +79,7 @@ def unitcon():
     else:
         distance_conv = distance_m
 
-    print("Converted distance:", distance_conv, end_unit)
+    print("\nConverted distance:", distance_conv, end_unit, "\n")
 
 
 def temperature():
@@ -247,7 +294,8 @@ while True:
 
     print ("1. Unit Conversion")
     print ("2. Finding for Pythagorean Theorem (Base, Leg, and Hypotenuse)")
-    print ("3. End the program")
+    print ("3. Triangle Assessment")
+    print ("4. End the program")
 
 
     choice = input('Input choice: ')
@@ -259,9 +307,12 @@ while True:
         pytha()
 
     elif choice == '3':
+        trianglecheck()
+
+    elif choice == '4':
         print ("-Program End-")
         break
         #Finishes Program
-        
+    
     else:
         print ("Choice Incomprehensible")
